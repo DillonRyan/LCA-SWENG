92b08913590f1e7b9277d1210d3f04027c0d1bf9
dillonryan98 <dillonryan98@gmail.com>
2019-09-19 15:04:59 +0100

    adding newNode

--- LCA.c
+++ LCA.c
@@ -7,7 +7,14 @@
  struct BNode {
    int data;
    struct BNode *left, *right;
- }
+ };
+
+  struct BNode * newNode(int data){
+    struct BNode * node = (struct BNode*)malloc(sizeOf(struct BNode));
+    node->data = data;
+    node->left = node->right = NULL;
+    return(node);
+  }
 
 int main(int argc, char const *argv[]) {
   /* code */
