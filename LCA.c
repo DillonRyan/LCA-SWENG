968401d34706c5af2a0cb7b62325d8c3596ce748
dillonryan98 <dillonryan98@gmail.com>
2019-09-19 14:52:50 +0100

    setting up

---
+++ LCA.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "math.h"
+
+
+ struct BNode {
+   int data;
+   struct BNode *left, *right;
+ }
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LCA-SWENG
+Repo for Software engineering Lowest common ancestor problem
+For this project i plan on using C as my language and Atom as my development environment
