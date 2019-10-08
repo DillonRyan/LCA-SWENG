4d937e33fa6f0faf9e190074be6cf54c93657e33
dillonryan98 <dillonryan98@gmail.com>
2019-10-08 14:07:48 +0100

    Working code but unsure how to write tests in C, very little amount of infromation online about how to do so

--- LCA.c
+++ LCA.c
@@ -1,22 +1,58 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "math.h"
+  #include "stdio.h"
+  #include "stdlib.h"
+  #include "string.h"
+  #include "math.h"
 
 
- struct BNode { // creating the template for the BST
-   int data; // value in node
-   struct BNode *left, *right; // pointer to the next node
- };
+   struct node { // creating the template for the BST
+     int data; // value in node
+     struct node *left, *right; // pointer to the next node
+   };
 
-  struct BNode * newNode(int data){ // function to create a new node in the BST
-    struct BNode * node = (struct BNode*)malloc(sizeof(struct BNode));  //creating the nodes memory 
-    node->data = data; //setting data
-    node->left = node->right = NULL;
-    return(node);
+    struct node * newNode(int data){ // function to create a new node in the BST
+      struct node * node = (struct node*)malloc(sizeof(struct node));  //creating the nodes memory
+      node->data = data; //setting data
+      node->left = node->right = NULL;
+      return(node);
+    }
+
+    struct node *lca(struct node* root, int n1, int n2)
+  {
+      if (root == NULL){
+        return NULL;
+      }
+
+      if (root->data > n1 && root->data > n2){
+          return lca(root->left, n1, n2);
+  }
+      if (root->data < n1 && root->data < n2){
+          return lca(root->right, n1, n2);
+  }
+      return root;
   }
 
-int main(int argc, char const *argv[]) {
-  /* code */
-  return 0;
-}
+  int main(int argc, char const *argv[]) {
+    struct node *root        = newNode(20);
+      root->left               = newNode(8);
+      root->right              = newNode(22);
+      root->left->left         = newNode(4);
+      root->left->right        = newNode(12);
+      root->left->right->left  = newNode(10);
+      root->left->right->right = newNode(14);
+
+      int n1 = 10, n2 = 14;
+      struct node *t = lca(root, n1, n2);
+      printf("LCA of %d and %d is %d \n", n1, n2, t->data);
+
+      n1 = 14, n2 = 8;
+      t = lca(root, n1, n2);
+      printf("LCA of %d and %d is %d \n", n1, n2, t->data);
+
+      n1 = 10, n2 = 22;
+      t = lca(root, n1, n2);
+      printf("LCA of %d and %d is %d \n", n1, n2, t->data);
+
+      getchar();
+    /* code */
+    return 0;
+  }
