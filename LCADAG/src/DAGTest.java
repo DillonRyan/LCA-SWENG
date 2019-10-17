11b2e97cfb0a186a822368928ce8dd618fbb5c76
dillonryan98 <dillonryan98@gmail.com>
2019-10-17 16:09:37 +0100

    adding final tests, testing for cycles then a big test to test LCA

--- LCADAG/src/DAGTest.java
+++ LCADAG/src/DAGTest.java
@@ -9,11 +9,11 @@ import org.junit.Test;
 	    public void testForAddingEdges() {
 
 	        DAG graph = new DAG(10);
-	        graph.addEdge(4,3);
-	        graph.addEdge(3,7);
-	        graph.addEdge(7, 5);
-	        graph.addEdge(5, 6);
-	        graph.addEdge(6, 8);
+	        graph.addEdge(4,3);//Connecting directed edge going from vertex 4 to vertex 3
+	        graph.addEdge(3,7);//Connecting directed edge going from vertex 3 to vertex 7
+	        graph.addEdge(7, 5);//Connecting directed edge going from vertex 7 to vertex 5
+	        graph.addEdge(5, 6);//Connecting directed edge going from vertex 5 to vertex 6
+	        graph.addEdge(6, 8);//Connecting directed edge going from vertex 6 to vertex 8
 
 	        assertEquals(5, graph.Edge()); // number of edges is 5
 	        assertEquals(10, graph.Vertices()); //number of vertices is 10
@@ -21,11 +21,11 @@ import org.junit.Test;
 		@Test
 			public void testForDirectedGraph(){
 				DAG graph = new DAG(10);
-				graph.addEdge(1, 2);
-				graph.addEdge(1, 3);
-				graph.addEdge(3, 4);
-				graph.addEdge(4, 5);
-				graph.addEdge(4, 6);
+				graph.addEdge(1, 2);//Connecting directed edge going from vertex 1 to vertex 2
+				graph.addEdge(1, 3);//Connecting directed edge going from vertex 1 to vertex 3
+				graph.addEdge(3, 4);//Connecting directed edge going from vertex 3 to vertex 4
+				graph.addEdge(4, 5);//Connecting directed edge going from vertex 4 to vertex 5
+				graph.addEdge(4, 6);//Connecting directed edge going from vertex 4 to vertex 6
 
 				assertEquals(0, graph.indegree(4));
 				assertEquals(2, graph.outdegree(4));
@@ -44,5 +44,112 @@ import org.junit.Test;
 				DAG graph = new DAG(5);
 				assertEquals(-1, graph.outdegree(8));
 			}
-		}
-	
+			@Test
+			public void testsForCycleInCyclic(){
+				DAG graph = new DAG(20);
+				graph.addEdge(0, 1);//Connecting directed edge going from vertex 0 to vertex 1
+				graph.addEdge(1, 2);//Connecting directed edge going from vertex 1 to vertex 2
+				graph.addEdge(2, 0);//Connecting directed edge going from vertex 2 to vertex 0
+				graph.findCycle(0);
+				assertTrue(graph.hasCycle());//Cycle from 0 - 1 - 2 - 0, which should return true.
+			}
+			@Test
+			public void testsForCycleInAyclic(){
+				DAG graph = new DAG(20);
+				graph.addEdge(0, 1); //Connecting directed edge going from vertex 0 to vertex 1
+				graph.addEdge(1, 3); //Connecting directed edge going from vertex 1 to vertex 3
+				graph.addEdge(2, 4); //Connecting directed edge going from vertex 2 to vertex 4
+				graph.findCycle(0);
+				assertFalse(graph.hasCycle()); //No Cycle,return false
+			}
+
+			 @Test
+			    public void testLowestCommonAncestor() {
+/*									10
+ * 								   /  \
+ * 								  /    \
+ * 								 /      \
+ * 								/        \
+ *                             ↙          ↘
+ * 							  5            15
+ *							  |             |
+ * 							  ꜜ			    ꜜ
+ * 							  3				20
+ * 							 / \			/ \
+ * 							/   \		   ↙   ↘
+ * 						   /     \		  13    14
+ * 						  ↙		  ↘
+ * 						 1		   2
+ * 								   /
+ * 								  /
+ * 								 ↙
+ * 								4
+ * 							   / \
+ * 							  /   \
+ * 							 ↙     ↘
+ * 							6       7
+ *
+ */
+
+
+			        DAG graph = new DAG(22);
+
+			        //starting at head with head = 10
+			        graph.addEdge(10, 5); //Connecting directed edge going from vertex 10 to vertex 5
+			        graph.addEdge(10, 15); //Connecting directed edge going from vertex 10 to vertex 15
+			        //going down the right branch
+			        graph.addEdge(15, 20); //Connecting directed edge going from vertex 15 to vertex 20
+			        graph.addEdge(20, 13); //Connecting directed edge going from vertex 20 to vertex 30
+			        graph.addEdge(20, 14); //Connecting directed edge going from vertex 20 to vertex 40
+			        //going down the left branch
+			        graph.addEdge(5, 3); //Connecting directed edge going from vertex 5 to vertex 3
+			        //going down the left left branch
+			        graph.addEdge(3, 1); //Connecting directed edge going from vertex 3 to vertex 1
+			        //going down the left right branch
+			        graph.addEdge(3, 2); //Connecting directed edge going from vertex 3 to vertex 2
+			        //going down the left right left branch
+			        graph.addEdge(2, 4); //Connecting directed edge going from vertex 2 to vertex 4
+			        graph.addEdge(4, 6); //Connecting directed edge going from vertex 4 to vertex 6
+			        graph.addEdge(4, 7); //Connecting directed edge going from vertex 4 to vertex 7
+
+			        //find LCA
+			        ArrayList<Integer> lca = graph.findLCA(1, 2);
+			        String result = "[3]";
+			        assertEquals(result, lca.toString());
+
+			        lca = graph.findLCA(1, 6);
+			        result = "[3]";
+			        assertEquals(result, lca.toString());
+
+			        lca = graph.findLCA(30, 30);
+			        result = "[30]";
+			        assertEquals(result, lca.toString());
+
+			        lca = graph.findLCA(6,7);
+			        result = "[4]";
+			        assertEquals(result, lca.toString());
+
+			        lca = graph.findLCA(1, 4);
+			        result = "[3]";
+			        assertEquals(result, lca.toString());
+
+			        lca = graph.findLCA(7,5);
+			        result = "[10]";
+			        assertEquals(result, lca.toString());
+
+			        lca = graph.findLCA(5,15);
+			        result = "[10]";
+			        assertEquals(result, lca.toString());
+
+			        lca = graph.findLCA(13,14);
+			        result = "[20]";
+			        assertEquals(result, lca.toString());
+
+			        lca = graph.findLCA(6,14);
+			        result = "[10]";
+			        assertEquals(result, lca.toString());
+
+			    }
+
+
+ }
